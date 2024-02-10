8123f759d4abae80e67cdff49ac1408a43d61b26
Dmitry Orlov <d.orlov777@gmail.com>
2024-02-10 18:02:42 +0300

    09-03: done

--- 09-rendering/03-UiPanes-JSX/components/UiPanes.vue
+++ 09-rendering/03-UiPanes-JSX/components/UiPanes.vue
@@ -1,50 +1,45 @@
-<template>
-  <div class="panes">
-    <!--  Pane 0 -->
-    <div class="pane">
-      <div class="pane__content">
-        <!-- Определяем, какой именно слот рендерить по массиву порядка панелей -->
-        <slot :name="`pane-${panes[0]}`" />
-      </div>
-      <div class="pane__controls">
-        <!-- Скрываем кнопку классом pane__disabled-button -->
-        <UiButton class="pane__disabled-button" variant="secondary" block @click="up(0)"> Up </UiButton>
-        <UiButton variant="danger" block @click="down(0)"> Down </UiButton>
-      </div>
-    </div>
-    <!--  Pane 1 -->
-    <div class="pane">
-      <div class="pane__content">
-        <slot :name="`pane-${panes[1]}`" />
-      </div>
-      <div class="pane__controls">
-        <UiButton variant="secondary" block @click="up(1)"> Up </UiButton>
-        <UiButton variant="danger" block @click="down(1)"> Down </UiButton>
-      </div>
-    </div>
-    <!--  Pane 2 -->
-    <div class="pane">
-      <div class="pane__content">
-        <slot :name="`pane-${panes[2]}`" />
-      </div>
-      <div class="pane__controls">
-        <UiButton variant="secondary" block @click="up(2)"> Up </UiButton>
-        <UiButton class="pane__disabled-button" variant="danger" block @click="down(2)"> Down </UiButton>
-      </div>
-    </div>
-  </div>
-</template>
-
 <script lang="jsx">
-// Предлагается решать задачу с использованием JSX, но вы можете использовать и чистые рендер-функции
-
 import UiButton from './UiButton.vue';
+import VNode from './VNode.vue';
 
 export default {
   name: 'UiPanes',
 
   components: {
     UiButton,
+  },
+
+  render() {
+    const vnodes = this.$slots.default();
+    return (
+      <div class="panes">
+        {this.panes.map((it, i) => (
+          <div class="pane">
+            <div class="pane__content">
+              <VNode vnode={vnodes[it]}></VNode>
+            </div>
+            <div class="pane__controls">
+              <UiButton
+                class={i === 0 ? 'pane__disabled-button' : ''}
+                variant="secondary"
+                block
+                onClick={() => this.up(i)}
+              >
+                Up
+              </UiButton>
+              <UiButton
+                class={i === vnodes.length - 1 ? 'pane__disabled-button' : ''}
+                variant="danger"
+                block
+                onClick={() => this.down(i)}
+              >
+                Down
+              </UiButton>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
   },
 
   data() {
@@ -54,7 +49,9 @@ export default {
        * [0, 1, 2]
        * @type {number[]|null}
        */
-      panes: [0, 1, 2],
+      panes: Array(this.$slots.default().length)
+        .fill()
+        .map((_, i) => i),
       // Сейчас здесь массив ровно из трёх элементов, но решение должно быть универсальным для любого количества узлов
     };
   },
