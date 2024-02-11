d2a2c876f4b4a7f3e6e8658c6b5d1139b77a4673
Dmitry Orlov <d.orlov777@gmail.com>
2024-02-11 16:26:56 +0300

    09-04: done

--- 09-rendering/04-AdvancedUiTabs-JSX/components/UiTabs.vue
+++ 09-rendering/04-AdvancedUiTabs-JSX/components/UiTabs.vue
@@ -1,8 +1,6 @@
 <script lang="jsx">
 // Предлагается решать задачу с использованием JSX, но вы можете использовать и чистые рендер-функции
 
-// import UiTab from './UiTab.vue';
-
 export default {
   name: 'UiTabs',
 
@@ -19,16 +17,26 @@ export default {
   },
 
   render() {
+    const tabNodes = this.$slots.default().sort(({ type: { name } }) => name === 'UiTab');
+
+    const tabButtons = tabNodes.map(({ props }) => (
+      <a
+        class={`tabs__tab ${this.$props.active === props.name ? ' tabs__tab_active' : ''}`}
+        onClick={() => this.setActive(props.name)}
+        role="tab"
+      >
+        {props.title}
+      </a>
+    ));
+
+    const activeTab = tabNodes.find(({ props }) => props.name === this.$props.active);
+
     return (
       <div class="tabs">
         <div class="tabs__nav" role="tablist">
-          <a class="tabs__tab" role="tab">Tab</a>
-          <a class="tabs__tab tabs__tab_active" role="tab">Active Tab</a>
-          <a class="tabs__tab" role="tab">Tab</a>
+          {tabButtons}
         </div>
-        <div class="tabs__content">
-          ACTIVE TAB CONTENT
-        </div>
+        <div class="tabs__content">{activeTab ? activeTab.children.default() : 'Tab content is not found'}</div>
       </div>
     );
   },
